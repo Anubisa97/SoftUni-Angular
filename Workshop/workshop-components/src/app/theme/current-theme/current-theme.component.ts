d0a143e0d95182018c2c5a7106a3ad918bc970be
Anubisa97 <assamsias97@gmail.com>
2024-03-22 05:45:56 +0200

    Finished this part of the Workshop

--- Workshop/workshop-components/src/app/theme/current-theme/current-theme.component.ts
+++ Workshop/workshop-components/src/app/theme/current-theme/current-theme.component.ts
@@ -1,21 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { ApiService } from 'src/app/api.service';
 import { Theme } from 'src/app/types/theme';
+import { UserService } from 'src/app/user/service.service';
 
 @Component({
   selector: 'app-current-theme',
   templateUrl: './current-theme.component.html',
-  styleUrls: ['./current-theme.component.css']
+  styleUrls: ['./current-theme.component.css'],
 })
 export class CurrentThemeComponent implements OnInit {
-  constructor(private apiService: ApiService) { }
-  theme = {} as Theme
+  constructor(
+    private apiService: ApiService,
+    private userService: UserService,
+    private activeRoute: ActivatedRoute
+  ) {}
+  theme = {} as Theme;
 
   ngOnInit(): void {
-    this.apiService.getTheme("5fa64a9f2183ce1728ff371a").subscribe((themes) => {
-      this.theme = themes
+    this.activeRoute.params.subscribe((data) => {
+      const id = data['themeId'];
+      this.apiService.getTheme(id).subscribe((themes) => {
+        this.theme = themes;
+        console.log(themes);
+      });
+    });
+  }
 
-      // this.theme = themes
-    })
+  get isLoggedIn(): boolean {
+    return this.userService.isLogged;
   }
 }
--- Workshop/workshop-components/src/app/theme/theme-routing.module.ts
+++ Workshop/workshop-components/src/app/theme/theme-routing.module.ts
@@ -1,16 +1,24 @@
-import { NgModule } from "@angular/core";
-import { RouterModule, Routes } from "@angular/router";
-import { MainComponent } from "./main/main.component";
-import { AddThemeComponent } from "./add-theme/add-theme.component";
-import { HomeComponent } from "../home/home.component";
-import { CurrentThemeComponent } from "./current-theme/current-theme.component";
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { AddThemeComponent } from './add-theme/add-theme.component';
+import { HomeComponent } from '../home/home.component';
+import { CurrentThemeComponent } from './current-theme/current-theme.component';
 
-const route: Routes = [{ path: 'themes', children: [{ path: '', pathMatch: 'full', component: MainComponent }, { path: ':themeId', component: CurrentThemeComponent }] }
-    , { path: 'add-theme', component: AddThemeComponent }, { path: 'home', component: HomeComponent }]
+const route: Routes = [
+  {
+    path: 'themes',
+    children: [
+      { path: '', pathMatch: 'full', component: MainComponent },
+      { path: ':themeId', component: CurrentThemeComponent },
+    ],
+  },
+  { path: 'add-theme', component: AddThemeComponent },
+  { path: 'home', component: HomeComponent },
+];
 
 @NgModule({
-    imports: [RouterModule.forChild(route)],
-    exports: [RouterModule]
+  imports: [RouterModule.forChild(route)],
+  exports: [RouterModule],
 })
-
-export class ThemeRoutingModule { }
+export class ThemeRoutingModule {}
